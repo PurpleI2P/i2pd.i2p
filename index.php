b85ba663d52478e1ce4b5ba037e4c1639783fae5
atnaguzin <atnaguzin@gmail.com>
2016-11-24 19:31:45 +0300

    banners

--- index.php
+++ index.php
@@ -13,27 +13,30 @@
 	<table border="0" width="100%" cellspacing="0" cellpadding="0" height="100%">
 		<tr>
 			<td align=center>
-				<img src="img/mask.png"><br/>
-				<font face="Arial" size="6">Your I2Pd Browser is working correctly</font><br/>
+				<img src="img/mask.png"><br>
+				<font face="Arial" size="6">Your I2Pd Browser is working correctly</font><br>
 				<font face="Arial" size="7">Welcome to Invisible Internet</font>
-				<br/><br/><br/><br/>
-				<font face="Arial" size="4">Visits: 
-					<?php
-						$f=fopen("stat.dat","a+");
-						flock($f,LOCK_EX);
-						$count=fread($f,100);
-						if (!isset($_COOKIE['i2pdvis'])){
-							@$count++;
-							ftruncate($f,0);
-							fwrite($f,$count);
-							fflush($f);
-							setcookie("i2pdvis", "true", time() + 1*3600, "/");
-						}
-						flock($f,LOCK_UN);
-						fclose($f);
-						echo $count; 
-					?>
-				</font>
+				<br><br><br>
+				<a href="http://102chan.i2p/" target="_blank"><img src="img/102button.gif" alt="102chan.i2p"></a>
+				<a href="http://333.i2p/" target="_blank"><img src="img/333.gif" width="88px" height="31px" alt="Форум, посвященный разработке и поддержке i2pd"></a>
+				<a href="http://flibusta.i2p/" target="_blank"><img src="img/flibusta.gif" width="88px" height="31px" alt="Флибуста - книжное братство"></a>
+				<a href="http://onelon.i2p/" target="_blank"><img src="img/onelon.png" alt="#onelon - анонимные блоги без цензуры"></a>
+				<a href="http://pizdabol.i2p/" target="_blank"><img src="img/pizdabol.gif" width="88px" height="31px" alt="Профессиональный эксперт"></a> 
+				<!-- <?php
+					$f=fopen("stat.dat","a+");
+					flock($f,LOCK_EX);
+					$count=fread($f,100);
+					if (!isset($_COOKIE['i2pdvis'])){
+						@$count++;
+						ftruncate($f,0);
+						fwrite($f,$count);
+						fflush($f);
+					}
+					setcookie("i2pdvis", "true", time() + 1*3600, "/");
+					flock($f,LOCK_UN);
+					fclose($f);
+					echo("<br><br><br><font face='Arial' size='3'>Visits: " .$count. "</font>"); 
+				?> -->
 			</td>
 		</tr>
 	</table>
