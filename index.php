649b8997fa26f86ace5f0adfdff8029203201940
orignal <i2porignal@yandex.ru>
2017-02-07 11:35:08 -0500

    wiki.ilita.i2p added

--- index.php
+++ index.php
@@ -29,7 +29,10 @@
 				<a href="http://dead.i2p/" target="_blank" title="Агрегатор новостей"><img src="img/dead.png" width="88px" height="31px" alt="dead.i2p"></a>
 				<a href="http://ptt.i2p/" target="_blank" title="Открытый торрент-трекер для анонимного использования"><img src="img/ptt.png" width="88px" height="31px" alt="ptt.i2p"></a>
 				<a href="http://r4sas.i2p/" target="_blank" title="Сервисы, работающие на I2Pd"><img src="img/r4sas.png" width="88px" height="31px" alt="r4sas.i2p"></a>
-				<a href="http://fsoc.i2p/" target="_blank" title="Социальная сеть FSOC"><img src="img/fsoc.png" width="88px" height="31px" alt="fsoc.i2p"></a>				
+				<a href="http://fsoc.i2p/" target="_blank" title="Социальная сеть FSOC"><img src="img/fsoc.png" width="88px" height="31px" alt="fsoc.i2p"></a>	
+				<br>
+				<a href="http://wiki.ilita.i2p/" target="_blank" title="Элитарная энциклопедия
+русского сегмента I2P"><img src="img/wiki_banner.png" width="88px" height="31px" alt="wiki.ilita.i2p"></a>	
 				<!-- <?php  // disabled
 					$f=fopen("stat.dat","a+");
 					flock($f,LOCK_EX);
